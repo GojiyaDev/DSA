ed830772a7ed4f94006b93426fc4ab8df1d1f30e
GojiyaDev <DEV.17103@stu.upes.ac.in>
2024-08-27 18:49:15 +0530

    Linear Search Program in C language

---
+++ LinearSearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1;  
+}
+
+int main() {
+    int n;
+
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int target;
+    
+    
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    
+    int result = linearSearch(arr, n, target);
+
+    
+    if (result == -1) {
+        printf("Element not found in the array.\n");
+    } else {
+        printf("Element found at index %d.\n", result);
+    }
+
+    return 0;
+}
